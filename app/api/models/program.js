1e7153f57876754bf0ca5bd4ad200a08d0a27990
Belloshehu <belloshehu1@gmail.com>
2023-07-13 10:00:51 +0100

    Define Schema for Program

---
+++ app/api/models/program.js
@@ -0,0 +1,87 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const ProgramSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Program title required"],
+      minLength: [5, "Title too short"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Program description required"],
+      minLength: [20, "Description too short"],
+      trim: true,
+    },
+    keyPersonName: {
+      type: String,
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: {
+        values: ["continous", "one-off"],
+        message: "{VALUE} is not a valid type for program",
+        required: [true, "Specify program type"],
+        default: "physical",
+      },
+    },
+    nature: {
+      // nature of program: virtual, hybrid or physical
+      type: String,
+      enum: {
+        values: ["virtual", "hybrid", "physical"],
+        message: "{VALUE} is not a valid type for program",
+        required: [true, "Specify program type"],
+        default: "physical",
+      },
+    },
+    virtualUrl: {
+      type: String,
+      default: "",
+    },
+    date: {
+      // for one-off programs, start date is specified
+      type: Date,
+    },
+    endDate: {
+      // for one-off programs, end date is specified
+      type: Date,
+    },
+    startTime: {
+      type: String,
+      required: [true, "Start time required"],
+    },
+    startStop: {
+      type: String,
+      required: [true, "Stop time required"],
+    },
+    days: {
+      // for contious program, days are specified instead of date
+      type: String,
+      enum: {
+        values: [
+          "sunday",
+          "monday",
+          "tuesday",
+          "wednesday",
+          "thursday",
+          "friday",
+          "saturday",
+        ],
+        message: "{VALUE} is not a valid day",
+      },
+    },
+    customDate: {
+      // incase days and start and end date do not match the type of date for the program
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Program ||
+  mongoose.model("Program", ProgramSchema);
